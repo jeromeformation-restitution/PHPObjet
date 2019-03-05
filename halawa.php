5382c5cb490994c1c52b8277aa2b8ffa0edd1a2f
jerome <ambroise.jerome.formation@gmail.com>
2019-03-05 09:25:08 +0100

    first_commit

---
+++ halawa.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<meta charset="utf-8">
+	<title></title>
+</head>
+<body>
+<p>je suis un paragraphe</p>
+<?php 
+
+echo "bonjour à tous";
+
+
+
+?>
+</body>
+</html>
